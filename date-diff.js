bd62ef6d4243f9a7457051c0adcb6af169bba462
Derek Batts <dbatts@republicwireless.com>
2018-04-19 17:50:53 -0400

    Fix years()

--- date-diff.js
+++ date-diff.js
@@ -53,8 +53,8 @@
 
   DateDiff.prototype.years = function() {
     var eom, ret;
-    ret = this.date1.getFullYear() - this.date2.getFullYear();
-    ret += (this.date1.getMonth() - this.date2.getMonth()) / 12;
+    ret = (this.date1.getMonth() - this.date2.getMonth()) / 12;
+    ret %= 12;
     eom = this.endOfMonth(this.date2).getDate();
     ret += (this.date1.getDate() / eom) - (this.date2.getDate() / eom);
     return this._roundIt(ret);
